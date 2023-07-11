aff64b02d1d66a05b061ce2e7cd231c5d2714105
AceDroidX <15519693+AceDroidX@users.noreply.github.com>
2023-07-11 16:58:52 +0800

    fix(douyin): 忽略部分用户信息的undefined

--- packages/shark7-douyin/src/event.ts
+++ packages/shark7-douyin/src/event.ts
@@ -27,7 +27,7 @@ export async function onUserDBEvent(ctr: MongoController, event: ChangeStreamUpd
         const isSkip = ['cover_url', 'white_cover_url', 'share_info'].some(value => key.startsWith(value))
         if (isSkip) return
         if ((flattenOrigin[key] != null && value == null) || (flattenOrigin[key] == null && value != null)) {
-            if(['city', 'commerce_user_info', 'country','cover_and_head_image_info','general_permission',
+            if(['city', 'commerce_user_info', 'country','ip_location','cover_and_head_image_info','general_permission',
             'life_story_block','original_musician','province','special_state_info','tab_settings',
             'urge_detail','video_icon'].some(value => key==value)) return
         }
