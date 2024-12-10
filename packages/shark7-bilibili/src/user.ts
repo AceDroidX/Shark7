22e26492e0ecc8845ca3c0fe268aafbd2679ef49
AceDroidX <15519693+AceDroidX@users.noreply.github.com>
2024-12-10 22:44:21 +0800

    fix(bilibili): 忽略undefined

--- packages/shark7-bilibili/src/user.ts
+++ packages/shark7-bilibili/src/user.ts
@@ -70,7 +70,7 @@ export async function onUserEvent(ctr: MongoController, event: ChangeStreamUpdat
             return
         }
         if ((flattenOrigin[key] != null && value == null) || (flattenOrigin[key] == null && value != null)) {
-            if (['live_room', 'elec', 'fans_medal.medal.wearing_status', 'user_honour_info.is_latest_100honour'].some(value => key == value)) return
+            if (['live_room', 'elec', 'fans_medal.medal.wearing_status', 'fans_medal.medal.today_feed', 'user_honour_info.is_latest_100honour'].some(value => key == value)) return
         }
         if (JSON.stringify(value) == '[]' || JSON.stringify(value) == '{}') {
             return
