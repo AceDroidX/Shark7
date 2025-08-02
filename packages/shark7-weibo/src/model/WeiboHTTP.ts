b38fd97b135f73e7fb362fd083e40bf85ce07ec8
AceDroidX <15519693+AceDroidX@users.noreply.github.com>
2025-08-02 14:49:54 +0000

    fix(weibo): 更新User-Agent并添加Referer头部

--- packages/shark7-weibo/src/model/WeiboHTTP.ts
+++ packages/shark7-weibo/src/model/WeiboHTTP.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { WeiboCookieMgr, cookieJsonToStr, logErrorDetail, logger } from "shark7-shared";
-const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36'
+const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Trailer/93.3.3570.29'
 
 export class WeiboHTTP {
     wcm: WeiboCookieMgr
@@ -9,7 +9,7 @@ export class WeiboHTTP {
     }
     async getURL<T = any>(url: string) {
         try {
-            return await axios.get<T>(url, { headers: { 'User-Agent': UA, 'cookie': cookieJsonToStr(this.wcm.cookie) } })
+            return await axios.get<T>(url, { headers: { "Referer": "https://weibo.com", 'User-Agent': UA, 'cookie': cookieJsonToStr(this.wcm.cookie) } })
         }
         catch (err) {
             if (axios.isAxiosError(err)) {
